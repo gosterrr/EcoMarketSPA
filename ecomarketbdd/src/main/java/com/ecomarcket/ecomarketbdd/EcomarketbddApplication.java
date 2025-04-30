414e87ca755edfc6c1b3aa5c5c335d5af90462a0
BEMENENES <169055821+BEMENENES@users.noreply.github.com>
2025-04-30 10:47:04 -0400

    Add files via upload

---
+++ ecomarketbdd/src/main/java/com/ecomarcket/ecomarketbdd/EcomarketbddApplication.java
@@ -0,0 +1,13 @@
+package com.ecomarcket.ecomarketbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketbddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketbddApplication.class, args);
+	}
+
+}
---
+++ ecomarketbdd/src/test/java/com/ecomarcket/ecomarketbdd/EcomarketbddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarcket.ecomarketbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomarketbddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
